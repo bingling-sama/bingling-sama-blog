41c83e63d17983ab162b142e3511cdd2568184ea
bingling_sama <bingling_sama@qq.com>
2020-12-13 20:21:02 +0800

    prepare

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: 'Booooling!',
+    description: '冰凌的个人博客辣',
+    theme: 'ting',
+    host: 'localhost',
+    port: 8090,
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico'}]
+        ['link', { rel: 'stylesheet', href: '/bingling.png'}]
+    ],
+    themeConfig: {
+        author: 'bingling_sama',
+        headImg: '/bingling.png',
+        catalog: '/catalog',
+        lastUpdated: '最近更新',
+        smoothScroll: true,
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '目录',link:'/config/catalog'},
+            { text: '项目列表', link: '/config/about' },
+            { text: 'Github',type:'url', link: 'https://github.com/bingling-sama' },
+        ],
+        footer: 'Made by bingling_sama | Powered by Vuepress | Themed by Ting'
+    }
+}
